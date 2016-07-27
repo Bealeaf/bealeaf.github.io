252c21bdbd12922b00353c972053cab4abce8c1f
Bealeaf <stefan.oliver.kuehnel@gmail.com>
2016-07-27 22:53:20 +0200

    CF
    v1.4.2

---
+++ cdn/php/contact/demo/assets/php/email.php
@@ -0,0 +1,10 @@
+<?php
+
+$mailTo = 'contact.lpsgroup@gmail.com';
+$mailFrom = '"LPS Group Mailserver" <mailserver@bealeaf.github.io>';
+$mailSubject    = 'Kontakt Formular';
+$returnPage = 'http://error.lpsgroup.bplaced.com/contact/success/';
+$returnErrorPage = 'http://error.lpsgroup.bplaced.com/contact/error/';
+$mailText = "";
+
+?>
